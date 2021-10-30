37ee1cd66c9f270c9654520d102ea674688006d9
Wong Ho Lung Ryu <sixmilk0128@gmail.com>
2021-10-30 16:24:50 +0100

    modified menu

--- src/Main.java
+++ src/Main.java
@@ -19,12 +19,13 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         //Sort list
-        System.out.println("Choose 1 sorting method: " +
-                "\n1. Bubble Sort " +
-                "\n2. Selection Sort " +
-                "\n3. Insertion Sort " +
-                "\n4. Merge Sort " +
-                "\n5. Quick Sort");
+        System.out.println("""
+                1. Bubble Sort\s
+                2. Selection Sort\s
+                3. Insertion Sort\s
+                4. Merge Sort\s
+                5. Quick Sort\s
+                Choose 1 sorting method:\s""");
         int choice = sc.nextInt();
         switch (choice){
             case 1:
@@ -32,18 +33,18 @@ public class Main {
                 processList.printList();
 
                 // TODO - rewrite the sorting method name
-            case 2:
-                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
-                processList.printList();
-            case 3:
-                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
-                processList.printList();
-            case 4:
-                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
-                processList.printList();
-            case 5:
-                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
-                processList.printList();
+//            case 2:
+//                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
+//                processList.printList();
+//            case 3:
+//                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
+//                processList.printList();
+//            case 4:
+//                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
+//                processList.printList();
+//            case 5:
+//                processList.setSortList(Sort.bubbleSort(processList.getList(), processList.getListSize()));
+//                processList.printList();
         }
 
 
